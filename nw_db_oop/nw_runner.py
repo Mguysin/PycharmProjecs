10e55aef87a31918e082103a5ff1b5f0f268de77
Marat Guysin <mguysin17@gmail.com>
2019-05-01 14:18:09 +0100

    average and encapsulated

--- nw_db_oop/nw_runner.py
+++ nw_db_oop/nw_runner.py
@@ -1,6 +1,7 @@
 from nw_products import NwProducts
 
 sql_runner = NwProducts()
-sql_runner.print_all_product_records()
+#sql_runner.print_all_product_records()
 
 sql_runner.print_average_unit_price()
+
